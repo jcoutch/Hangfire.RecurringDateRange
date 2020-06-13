0faec6266480c3f5dcefb0590d51943f53c209e9
Craig Brown <craig.brown@maritzcx.com>
2020-06-12 21:25:22 -0400

    Add missing files

---
+++ src/Hangfire.RecurringDateRange/Constants/HashKeys.cs
@@ -0,0 +1,14 @@
+﻿namespace Hangfire.RecurringDateRange.Constants
+{
+    public static class HashKeys
+    {
+        public const string Cron = "Cron";
+        public const string EndDate = "EndDate";
+        public const string UseEndDateTimeComponent = "UseEndDateTimeComponent";
+        public const string Job = "Job";
+        public const string LastExecution = "LastExecution";
+        public const string NextExecution = "NextExecution";
+        public const string StartDate = "StartDate";
+        public const string TimeZoneId = "TimeZoneId";
+    }
+}
